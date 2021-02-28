1f87cd87b4ecbaa105f8edfcaba0a1af6ad24f6c
Jason Harris <Harris-J@users.noreply.github.com>
2021-02-28 13:48:11 -0700

    sunday work

--- scraper.py
+++ scraper.py
@@ -1,29 +1,33 @@
 #uses Requests and BeautifulSoup 
 #PIP those in.
-
+# sample ISBN 9780276440502
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.abebooks.com/servlet/SearchResults?cm_sp=sort-_-SRP-_-Results&ds=100&kn=9780340837245&sortby=2'
+#Build the URL
+base ='https://www.abebooks.com/servlet/SearchResults?sts=t&cm_sp=SearchF-_-home-_-Results&ds=100&an=&tn=&kn=&isbn='
+#ISBN =  input("Give us ISBN: ")
+ISBN = '9780276440502'
+
+URL = base + ISBN
 page = requests.get(URL)
-
 soup = BeautifulSoup(page.content, 'html.parser')
-
+type(soup)
 ## IDs we'll need
 ##id="topbar-search-result-count"
 
-## Results 
+## Get all the results 
 books = soup.find(id='main')
 
-## Find Elements by HTML Class Name
-# example job_elems = results.find_all('section', class_='card-content')
-#<div class="srp-item-price" id="srp-item-price-2">US$ 1.03</div>
-# for job_elem in job_elems:
-#     print(job_elem, end='\n'*2)
-price_elements = books.find_all('div', class_= 'srp-item-price')
+## Find price elements by HTML Class Name
 
-for prices in price_elements:
-    print(prices, end='\n'*2)
+price_elements = soup.find_all('div', class_= 'srp-item-price')
+print(type(price_elements))
+print(len(price_elements))
+print(price_elements)
+
+## Get a single price for book 1
+
 
 # ## Output 
 # print("Total books")
---
+++ scratch_pad.py
@@ -0,0 +1,15 @@
+#tutorials used:
+https://www.dataquest.io/blog/web-scraping-beautifulsoup/
+https://realpython.com/python-web-scraping-practical-introduction/
+
+
+## Find Elements by HTML Class Name
+# example job_elems = results.find_all('section', class_='card-content')
+#<div class="srp-item-price" id="srp-item-price-2">US$ 1.03</div>
+# for job_elem in job_elems:
+#     print(job_elem, end='\n'*2)
+
+
+
+
+
