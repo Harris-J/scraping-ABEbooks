0b263710b6a4240264f35af57c233efb09ab6a5d
Jason Harris <Harris-J@users.noreply.github.com>
2021-03-04 19:26:15 -0700

    Cleaned up (committ on 'main')

--- scratch_pad.py
+++ scratch_pad.py
@@ -10,6 +10,14 @@
 #     print(job_elem, end='\n'*2)
 
 
+## Find price elements by HTML Class Name
+#book_elements = soup.find_all('li', class_= 'cf result-item')
+## Get a single price for book 1
+#single_book = book_elements[25]
+#print(single_book)
+
+# title = single_book.find("meta",  itemprop="author")
+# print(title["content"])
 
 #  <div class="srp-item-price" id="srp-item-price-1">US$ 1.00</div>
 #  title = soup.find("meta",  property="og:title")
@@ -21,6 +29,17 @@
 # sample ISBN 9780276440502
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+import pandas as pd
+lst1 = range(10)
+lst2 = range(10)
+lst3 = range(10)
+percentile_list = pd.DataFrame(
+    {'lst1Title': lst1,
+     'lst2Title': lst2,
+     'lst3Title': lst3
+    })
+print(percentile_list)
 
 #Build the URL
 base ='https://www.abebooks.com/servlet/SearchResults?sts=t&cm_sp=SearchF-_-home-_-Results&ds=100&an=&tn=&kn=&isbn='
